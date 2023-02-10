4f354ebb2024c6f3338511562ef02b1213c03cfa
jarrodmjack <jarrodmjack@gmail.com>
2023-02-09 20:47:06 -0500

    add timepunch table

--- backend/creativepay/views.py
+++ backend/creativepay/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from .models import Employee, TimePunch
 from .serializers import EmployeeSerializer, TimePunchSerializer
 
@@ -10,3 +12,12 @@ class EmployeeAPIView(viewsets.ModelViewSet):
 class TimePunchAPIView(viewsets.ModelViewSet):
     queryset = TimePunch.objects.all()
     serializer_class = TimePunchSerializer
+    
+    def create(self, request):
+        print(request.data)
+        serializer = TimePunchSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            response = Response(serializer.data)
+            return response
+        return Response(serializer.errors, status=400)
